d6bbe2a1844f60be09a09e75fe74aadf382d5686
johnnyquinn <jftq97@gmail.com>
2021-02-08 16:22:26 -0500

    [add] contents to Event and guest_event_table models

--- events_app/models.py
+++ events_app/models.py
@@ -29,9 +29,15 @@ class Guest(db.Model):
 class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.String(100))
+    date_and_time = db.Column(db.Date)
+    guests = db.relationship('Guest', back_populates='events_attending')
 
 # TODO: Create a table `guest_event_table` with the following columns:
 # - event_id: Integer column (foreign key)
 # - guest_id: Integer column (foreign key)
 
-guest_event_table = None
+guest_event_table = db.Table('guest_event_table',
+    db.Column('event_id', db.Integer, db.ForeignKey('event.id')),
+    db.Column('guest_id', db.Integer, db.ForeignKey('guest.id'))
+)
